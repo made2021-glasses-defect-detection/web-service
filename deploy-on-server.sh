51f71f63ae49d14d1be30aaa9812c48d78887bba
Alex Emelyanov <holyketzer@gmail.com>
2021-12-22 15:04:20 +0000

    Fix deploy script

--- deploy-on-server.sh
+++ deploy-on-server.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+git pull
 
 docker-compose -f docker-compose.yml pull
-docker-compose -f docker-compose.yml down
+docker-compose -f docker-compose.yml down --remove-orphans
 docker-compose -f docker-compose.yml up -d
-docker image prune
+
+docker image prune -f
